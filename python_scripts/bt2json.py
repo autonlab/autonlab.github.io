57ba446f130021dd67f1f0bd7317390ad8f0d957
Andrew Williams <awillia2@lula.auton.cs.cmu.edu>
2020-08-06 11:05:16 -0400

    Added Dockerfile and such for bibtex parsing

---
+++ python_scripts/bt2json.py
@@ -0,0 +1,60 @@
+import bibtexparser
+import json
+from bibtexparser.customization import convert_to_unicode
+import re
+import sys
+import glob
+from os.path import join
+
+
+count = 0
+scount = 0
+entry = re.compile('^@')
+
+worker = None
+old = None
+
+bibs = []
+bids = {}
+
+for f in glob.glob('/publications/*.bib'):
+    
+    with open(join('/publications', f)) as bf:
+        parser = bibtexparser.bparser.BibTexParser()
+        parser.customization = convert_to_unicode
+
+        for line in bf:
+            if (entry.match(line)):
+                count += 1
+                old = worker
+                worker = line
+                if old:
+                    bdict = bibtexparser.loads(old)
+                    if (bdict.entries):
+                        scount+=1
+                        bibs.append(bdict.entries[0])
+                        if (scount == count):
+                            print("Mismatch at: " + str(count))
+                    else:
+                        print("Failure to parse entry: " + str(count))
+                        print(old)
+            else:
+                if worker:
+                    worker = worker + line
+    
+if worker:
+    bdict = bibtexparser.loads(worker)
+    if (bdict.entries):
+        scount+=1
+        bibs.append(bdict.entries[0])
+    else:
+        print("Failure to parse entry: " + str(count))
+            
+if (count != scount):
+    print("Failed: " + str(count) + " entries and " + str(scount) + " parsed.")
+    sys.exit(count - scount)
+
+with open ('publications.json','w') as jd:
+    json.dump(bibs,jd)
+
+sys.exit(0)
