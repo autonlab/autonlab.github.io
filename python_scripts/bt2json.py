b9706d44f9ed891cee7f152e63a5ed2fdcc02a51
Andrew Williams <awillia2@lula.auton.cs.cmu.edu>
2020-08-24 11:39:08 -0400

    Added error documentation to the bibtex parsing

--- python_scripts/bt2json.py
+++ python_scripts/bt2json.py
@@ -17,6 +17,8 @@ old = None
 bibs = []
 bids = {}
 
+err = open('data/bibtexerrors.txt','w')
+
 for f in glob.glob('publications/*.bib'):
     
     with open(f) as bf:
@@ -36,8 +38,8 @@ for f in glob.glob('publications/*.bib'):
                         if (scount == count):
                             print("Mismatch at: " + str(count))
                     else:
-                        print("Failure to parse entry: " + str(count))
-                        print(old)
+                        err.write("Failure in file: " + f + '\n')
+                        err.write(old + '\n')
             else:
                 if worker:
                     worker = worker + line
@@ -48,7 +50,9 @@ if worker:
         scount+=1
         bibs.append(bdict.entries[0])
     else:
-        print("Failure to parse entry: " + str(count))
+        err.write("Failure in file: " + f + '\n')
+        err.write(worker + '\n')
+
             
 if (count != scount):
     print("Failed: " + str(count) + " entries and " + str(scount) + " parsed.")
